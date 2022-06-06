9737e8d46d7639841e51cc03a2819c66510455f5
ios10 <trikshotzz05@gmail.com>
2022-06-05 19:55:36 -0700

    more done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -19,21 +19,21 @@
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
 // create a describe it test that takes in tow arrays and expects the output to be the first of the array taken out and the rest of the values mixed
-    describe("unorderedArray", () => {
-        const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-        const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-        it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
-     const expected1 = ["yellow", "blue", "pink", "green"]
-     const expected2 = ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
-            expect(unorderedArray(colors1)).toEqual(expect.arrayContaining(expected1))
-            expect(unorderedArray(colors2)).toEqual(expect.arrayContaining(expected2))
-        })
-        // it('does not match if the first value is still in the array.', () => {
-        //     expect(unorderedArray(colors1)).not.toEqual(expect.arrayContaining(expected1))   <----- I didn't understand if i was supposed to have this .not expect. It just told me tha expected1 was not defined, but when I leave this out, it works.
+    // describe("unorderedArray", () => {
+    //     const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+    //     const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+    //     it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+    //  const expected1 = ["yellow", "blue", "pink", "green"]
+    //  const expected2 = ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+    //         expect(unorderedArray(colors1)).toEqual(expect.arrayContaining(expected1))
+    //         expect(unorderedArray(colors2)).toEqual(expect.arrayContaining(expected2))
+    //     })
+    //     // it('does not match if the first value is still in the array.', () => {
+    //     //     expect(unorderedArray(colors1)).not.toEqual(expect.arrayContaining(expected1))   <----- I didn't understand if i was supposed to have this .not expect. It just told me tha expected1 was not defined, but when I leave this out, it works.
         
-        //     expect(unorderedArray(colors2)).not.toEqual(expect.arrayContaining(expected2))
-        // })
-    })
+    //     //     expect(unorderedArray(colors2)).not.toEqual(expect.arrayContaining(expected2))
+    //     // })
+    // })
 
     // i believe i failed correctly 
     //  ● unorderedArray › takes in an array, removes the first item from the array and shuffles the remaining content
@@ -61,13 +61,13 @@
 // ["yellow", "blue", "pink", "green"]
 // ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
-    const unorderedArray = (array) => {
-        array.shift()
-        for(let i=0; i <= array.length; i++){
-        let newArray = array.sort(() => Math.random() * i)
-        return newArray
-    } 
-}
+//     const unorderedArray = (array) => {
+//         array.shift()
+//         for(let i=0; i <= array.length; i++){
+//         let newArray = array.sort(() => Math.random() * i)
+//         return newArray
+//     } 
+// }
 
 // I passed
 //  PASS  ./code-challenges.test.js
@@ -84,14 +84,14 @@
 
 // a) Create a test with expect statements for each of the variables provided.
     // create a describe it test that takes in an array of numbers and returns a new array with a number of least and greatest value.
-        describe('twoArray', () => {
-            it("takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
-                const nums1 = [3, 56, 90, -8, 0, 23, 6]
-                const nums2 = [109, 5, 9, 67, 8, 24]
-                expect(twoArray(nums1)).toEqual([-8, 90])
-                expect(twoArray(nums2)).toEqual([5, 109])
-            })
-        })
+        // describe('twoArray', () => {
+        //     it("takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
+        //         const nums1 = [3, 56, 90, -8, 0, 23, 6]
+        //         const nums2 = [109, 5, 9, 67, 8, 24]
+        //         expect(twoArray(nums1)).toEqual([-8, 90])
+        //         expect(twoArray(nums2)).toEqual([5, 109])
+        //     })
+        // })
 
         // I failed
         // ● twoArray › takes an array of numbers and returns an array of the minimum and maximum numbers in that order
@@ -108,19 +108,21 @@
 // b) Create the function that makes the test pass.
 
         // create a function called twoArray
-        // function will take in a conditional
-        // if the number of the .indexOf is greater than -8 or less than 90 then none of those numbers will be output.
+        // create a variable withe a empty array
         // sort the new array for least to greatest with .sort()
+        // function will take in a conditional
+        // if the number of the .indexOf is greater than the index of 0 or less than the array length -1 then none of those numbers will be output.
+        
 
-            const twoArray = (order) => {
-                let emptyArray = []
-                let thisArray = order.sort((a, b) => a - b)
-                for(let i =0; i <= thisArray.length; i++){
-                    if(thisArray.indexOf[i] > thisArray.indexOf[0] || thisArray.indexOf[i] < thisArray[thisArray.length -1] ){
-                         emptyArray.push(thisArray)
-                    }
-                } return emptyArray
-            }
+            // const twoArray = (order) => {
+            //     let emptyArray = []
+            //     let thisArray = order.sort((a, b) => a - b)
+            //     for(let i =0; i <= thisArray.length; i++){
+            //         if(thisArray.indexOf[i] > thisArray.indexOf[0] || thisArray.indexOf[i] < thisArray[thisArray.length -1] ){
+            //              emptyArray.push(thisArray)
+            //         }
+            //     } return emptyArray
+            // }
             // const twoArray = (order) => {
             //     let newArray = []
             //     let thisArray = order.sort((a, b) => a - b)
@@ -150,17 +152,45 @@
             //     -   -8,
             //     -   90,
             //     - ]
-            //     + Array [] I can't find why I keep getting this 
+            //     + Array [] I can't find why I keep getting this extra array?
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-            // create a descrite it test that will take two arrays a s arguments adn only one will be return one array with non duplicate values.
+            // create a describe it test that will take two arrays as arguments and only one will be return one array with non duplicate values.
+    describe("oneArray", () => {
+        it("takes in two arrays as arguments and returns one array with no duplicate values", () => {
+            const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+            const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+            expect(oneArray(this)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+        })
+    })
 
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+    // I failed
+    // ● oneArray › takes in two arrays as arguments and returns one array with no duplicate values
+
+    // ReferenceError: oneArray is not defined
+
+// const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+// const testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
 // b) Create the function that makes the test pass.
+
+    // create ac function called oneArray
+    //  function will take in two arrays with "this"
+    //create a for loop to iterate through each array
+    // if any index matches any other index, it will not be output
+    // concat the remaining values
+
+        // const oneArray = (one) => {
+        // let thisArray = one.concat()
+        //      for(let i = 0; i < one.length; i++){
+        //     }
+        // }
+        const oneArray = (one) => {
+            let thisArray = one.concat()
+        let uniqueChars = [...new Set(thisArray)]
+        } 
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,7 +10,21 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+#create a method called numbers
+# if the number % 2 is equal to 0 then print that is is even with interpolation #{}
+# if it is not then print that it is odd
 
+def numbers(which) 
+    if which % 2 == 0 
+          "#{which} is even"
+    else
+         "#{which} is odd "
+    end
+end
+
+p numbers(num1) 
+p numbers(num2) 
+p numbers(num3) 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
@@ -21,8 +35,22 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def vowels(none)
+    str_arr = none.split("")
+    vowels = "Aaeiou"   #did this 'A'..'z' and realized it took everything out😂
+    i = 0
+    while i < none.length
+      if vowels.include?(none[i])
+        str_arr[i] =  ""
+      end
+      i +=1
+    end
+    new_string = str_arr.join
+end
 
-
+p vowels(album1)
+p vowels(album2)
+p vowels(album3)
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
 palindrome_tester1 = 'Racecar'
@@ -31,3 +59,16 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+
+def palin string
+    if string == string.reverse.capitalize
+        "#{string} is a Palindrom"
+    else
+        "#{string} is not a palindrome"
+    end
+end
+
+p palin(palindrome_tester1)
+p palin(palindrome_tester2)
+p palin(palindrome_tester3)
